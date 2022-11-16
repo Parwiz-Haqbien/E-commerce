436bbc6e47544fadf0e30a52f43161a3c6c8e221
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-11-16 15:53:52 +1100

    index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreinKey: 'category_id',
+});
 
 // Categories have many Products
+Product.hasMany(Product, {
+  foreinKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreinKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreinKey: 'tag_id',
+});
 
 module.exports = {
   Product,
